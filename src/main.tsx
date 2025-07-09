08deb50a0c5c3e37b4e462a6224128407b4a8bc3
Brian Stroka <brian@macbook-air-3.local.ftr>
2025-07-09 10:59:21 -0400

    Update main app entry point and fix OpenAI warnings

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
-import App from './App-DirectLogin';
+import App from './App';
 
 // Inject Google Font "Cinzel" for a more biblical look
 (() => {
